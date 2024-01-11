24c4c7a22d400fcdf4202134a47f2f79cace83a6
Mestiri77 <faroukmestiri77@gmail.com>
2024-01-11 11:23:43 +0100

    first warmup done

--- warmup01.js
+++ warmup01.js
@@ -1,40 +1,60 @@
-// 1-using + operator create a function that combine your partner first and last name and return it.*//
+// 1-using + operator create a function 
+//that combine your partner first and last name and return it.*//
 
 // function declaration
 function MyPartnerName(firstname, lastname){
-// write yourcode 
+		// return firstname+" "+lastname  
+		return firstname.concat(" ",lastname)
    }
 // function expression 
 
 let myPartnerName=function(firstname,lastname){
-//write your code
+	return firstname.concat(" ",lastname)
+
 }
 
 // arrow function 
 
-let PartnerName2 = (firstName,lastName) => {
-// write your code 
-}
+let PartnerName2 = (firstName,lastName) => firstName.concat(" ",lastName)
 
-// 2-create a function that return true if the number 13 is a multiple of 3 and false if not.*//
+// 2-create a function that return true 
+// if the number 13 is a multiple of 3 and false if not.*//
 
 function MultipleOf13orNot(){
-	//write your code
+	if(13%3 === 0){
+		return true
+	}
+	else { return false }
 }
 // function expression 
-
+ let MultipleOf13orNot = function(){
+	if(13%3 === 0){
+		return true
+	}
+	else { return false }
+ }
 
 
 // arrow function 
+let MultipleOf13orNot2 = ()=> {
+	if(13%3 === 0){
+		return true
+	}
+	else { return false }
+}
 
 // 4-create a function that takes a age as a parametre and return that age in seconds.*//
 
 function AgeInSeconds(age){
-	//write your code
+	return age*365*24*60*60
 }
 
 // function expression 
 
-
+let AgeInSeconds=function(age) {
+	return age*365*24*60*60
+}
 
 // arrow function 
+  
+let AgeInSeconds1 = age => age*365*24*60*60
