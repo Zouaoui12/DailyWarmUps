ad26c9d3cc3739ee9577e7133e3b5f2ce6c8ac33
Farouk Mestiri <117433957+Mestiri77@users.noreply.github.com>
2024-01-11 08:57:20 +0100

    Create warmup01.js
    First warmup

---
+++ warmup01.js
@@ -0,0 +1,40 @@
+// 1-using + operator create a function that combine your partner first and last name and return it.*//
+
+// function declaration
+function MyPartnerName(firstname, lastname){
+// write yourcode 
+   }
+// function expression 
+
+let myPartnerName=function(firstname,lastname){
+//write your code
+}
+
+// arrow function 
+
+let PartnerName2 = (firstName,lastName) => {
+// write your code 
+}
+
+// 2-create a function that return true if the number 13 is a multiple of 3 and false if not.*//
+
+function MultipleOf13orNot(){
+	//write your code
+}
+// function expression 
+
+
+
+// arrow function 
+
+// 4-create a function that takes a age as a parametre and return that age in seconds.*//
+
+function AgeInSeconds(age){
+	//write your code
+}
+
+// function expression 
+
+
+
+// arrow function 
