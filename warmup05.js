d9a4911cee190c1003faa9625387d58904e8e1df
zouaoui12 <Zouaouisabrine31@gmail.com>
2024-01-17 10:16:09 +0100

    done 30

--- warmup05.js
+++ warmup05.js
@@ -1,6 +1,13 @@
 //1.Write a function to print all the even numbers befor the given parameter.
 
 function printEven(num) {
+    let result = 0
+    for (let i = 0 ; i= %num; ; i+=2) {
+       let result += i ;
+        
+    } 
+    return result
+
   
 }
 
@@ -8,7 +15,12 @@ function printEven(num) {
 //2.Write a function to find the power of a number using a for loop.
 
 
-function findPower(base, exponent) {
+function findPower(base, exponent) { 
+    let power = 1 
+    for (let i = 0; i <=0 ; i++) {
+        const element = array[index];
+        
+    }
 }
 
 
@@ -17,6 +29,7 @@ function findPower(base, exponent) {
 //takes a number as a parameter and returns the sum of the first and last digits. Please do this without turning the number into a string.
 
 function sumOfFirstAndLast(number) {
+  
  
 } 
 
@@ -24,5 +37,11 @@ function sumOfFirstAndLast(number) {
 //4.Write a function called multiplyBy10NTimes that takes two numbers as parameters and returns the first number multiplied by 10 the amount of times indicated by the second number.
 
 function multiplyBy10NTimes(num, n) {
+    let result =1
+    for (let i = 1 ; i<=n; i++) {
+        result*= n ;
+        
+    } 
+    return result
  
 } 
