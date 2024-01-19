eb5700810c80142b150d668b45f22c61d82a903d
Farouk Mestiri <117433957+Mestiri77@users.noreply.github.com>
2024-01-19 08:50:00 +0100

    warmup07.js

---
+++ warmup07.js
@@ -0,0 +1,53 @@
+/**
+ * 1- write a function to Reverse an array without use array.reverse() method:
+ * ex: reverseArray([1,2,3]) ==> [3,2,1]
+ * @function reverseArray
+ * @param {array} array
+ * @return {Array}
+ */
+
+let reverseArray = (array ) => {
+
+}
+
+/**
+ * 2 - Write a function named objKey that take an object as a parameter and returns an array containing the keys of the object
+ * objKey({name:'ahmed',age:20}) ==> ['name','age']
+ *
+ * @function objKey
+ * @param {Object} obj
+ * @return {Array}
+ */
+
+let objKey=(object)=>{
+   
+}
+
+/**
+ * 3 - Write a function named objLength to get the length of a JavaScript object
+ * objLength({name:'ahmed',age:20}) ==> 2
+ *
+ * @function objLength
+ * @param {Object} obj
+ * @return number
+ */
+let objLength=(obj) =>{
+
+}
+
+
+/**
+ * 4-Given an array of integers and an integer n.
+ * find all occurrences of n in the given array and return another array containing all the index positions of n in the given array.
+ * If n is not in the given array, return an empty array [].
+ * Assume that n and all values in the given array will always be integers.
+ * Example:
+ * findAll([6, 9, 3, 4, 3, 82, 11], 3) => [2, 4]
+ *
+ * @param {Array} array
+ * @param {number} n
+ * @return {Array}
+ */
+let findAll = function (array, n) {
+  
+};
