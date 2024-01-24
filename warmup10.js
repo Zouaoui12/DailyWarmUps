ec79c99a27d2a5f81a4b2c197e513d4ce56b556c
Farouk Mestiri <117433957+Mestiri77@users.noreply.github.com>
2024-01-24 09:05:12 +0100

    Create warmup10.js

---
+++ warmup10.js
@@ -0,0 +1,61 @@
+//1- write a function that takes a string as an argument
+// and returns the string reversed word by word
+// Example : reverseStr("hi there how is the weather today") ==>> "today weather the is how there hi"
+//
+//
+// Note : you can`t use any built-in functions .
+
+function reverseStr(str){
+ } 
+
+
+
+
+/**
+ *2- Given an array of integers and an integer n.
+ * find all occurrences of n in the given array and return another array containing all the index positions of n in the given array.
+ * If n is not in the given array, return an empty array [].
+ * Assume that n and all values in the given array will always be integers.
+ * Example:
+ * findAll([6, 9, 3, 4, 3, 82, 11], 3) => [2, 4]
+ *
+ * @param {Array} array
+ * @param {number} n
+ * @return {Array}
+ */
+
+let findAll = function (array, n) {
+ 
+};
+
+
+/**
+ * 4 - Write a function named objSort to sort an array of JavaScript objects based on the id
+ * objSort([{id:2,value:50},{id:0,value:70},{id:1,value:40}]) ==> [{id:0,value:70},{id:1,value:40},{id:2,value:50}]
+ *
+ * @function objSort
+ * @param {Array<{id: number, value: number}>} obj
+ * @return {Array}
+ */
+
+
+function objSort(array){
+
+    
+}
+
+
+
+/** write a closure function called Counter that have 2 behaviours and a state to check the counter and when invoked it increase or decrease the counter. 
+//!! ps : the function should return an object ! 
+ex : let myCount = Counter();
+myCount.up() => 1 
+myCount.up() => 2 
+myCount.down() => 1 
+myCount.counter => 1 
+**/ 
+
+let Counter = () => {
+  
+}
+
