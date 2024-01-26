162ffb6102d07f761d8f5a484aebf4a8f925dbbe
Farouk Mestiri <117433957+Mestiri77@users.noreply.github.com>
2024-01-26 09:24:06 +0100

    Recrusion+oop

---
+++ warmup12.js
@@ -0,0 +1,62 @@
+ // Write a JavaScript function that will iterate from n to 0. For each iteration,
+ // it will check if the current number is odd or even, and display a message to the screen.
+ // iterateAndLog(3); =>
+ // Sample Output :
+ // "2 is even"
+ // "1 is odd"
+ // "0 is even"
+
+function iterateAndLog(n) {
+
+  // TODO: your work goes here
+
+}
+
+ // Write a JavaScript function that will iterate from n to 0. For each iteration,
+ // it will check if the current number is odd or even, and display a message to the screen.
+ // iterateAndLog(3); =>
+ // Sample Output :
+ // "2 is even"
+ // "1 is odd"
+ // "0 is even"
+
+function iterateAndLog(n) {
+
+  // TODO: your work goes here
+
+}
+
+
+// The Reading List 
+// An object-oriented book-list!
+
+// Create a class BookList
+
+// Create another class called Book
+
+// BookLists should have the following properties:
+
+// Number of books marked as read
+// Number of books marked not read yet
+// A reference to the next book to read (book object)
+// A reference to the current book being read (book object)
+// A reference to the last book read (book object)
+// An array of all the Books
+// Each Book should have several properties:
+
+// Title
+// Genre
+// Author
+// Read (true or false)
+// Read date, can be blank, otherwise needs to be a JS Date() objectLinks to an external site.
+// Every Booklist should have a few methods:
+
+// .add(book)
+// should add a book to the books list.
+// .finishCurrentBook()
+// should mark the book that is currently being read as "read"
+// Give it a read date of new Date(Date.now())
+// Change the last book read to be the book that just got finished
+// Change the current book to be the next book to be read
+// Change the next book to be read property to be the first unread book you find in the list of books
+// Booklists and Books might need more methods than that. Try to think of more that might be useful.
